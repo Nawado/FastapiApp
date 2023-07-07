8db33a2b295ab691bf50e11f53ef1ff55f9ec3b9
U_M191W <glebivanchenko@bayestech.ru>
2023-07-07 18:28:18 +0300

    added tables

---
+++ app/db/tables/exchanges.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, func
+from sqlalchemy.dialects.postgresql import ARRAY
+
+metadata = sqlalchemy.MetaData()
+
+exchanges_table = sqlalchemy.Table(
+    'exchanges',
+    metadata,
+    Column('id', Integer, primary_key=True, index=True),
+    Column('name', String),
+	Column('code', String),
+	Column('country', String),
+	Column('currency', String),
+	Column('countryIso2', String),
+	Column('countryIso3', String),
+	Column('tickers', String)
+)
